2404eff3a1d7d76e8b5880bad40630ece3d090ae
TranDucKhiem14082005 <khiemtran015@gmail.com>
2024-09-19 22:30:59 +0700

    review: JavaScript-Day1

---
+++ On_tap/review-js/main.js
@@ -0,0 +1,54 @@
+
+// var fullName = 'Tran Duc Khiem';
+// var age = 19;
+// alert(fullName);
+// alert(age)
+// console.log(fullName);
+
+/*
+// Built-in function: Hàm đã được định nghĩa sẵn
+
+    1.Alert
+    2.Console
+    3.Confirm: the same to Alert but it must us choice
+    cancel or ok for identify
+    4.Prompt: the same to Confirm but it has input for me write
+    information into it
+    5.Set timeout
+    6.Set interval
+
+*/
+
+var fullName = 'Tran Duc Khiem';
+
+console.log(fullName);
+
+setTimeout(function () {
+    console.log(fullName)
+},1000)
+
+
+/*
+    Toán tử trong JavaScript
+    1. Toán tử số học
+    2. Toán tử gán
+    3. Toán tử so sánh
+    4. Toán tử logic
+*/
+
+/*
+    Toán tử số học
+    +
+    -
+    *
+    **: Lũy thừa
+    /
+    %
+    ++
+    --
+*/
+
+const a = 1;
+const b = 2;
+const c = a + b;
+console.log(c);
