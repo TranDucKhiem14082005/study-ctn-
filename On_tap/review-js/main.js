ee558d72dfd84530cf66ca4dfad2b073e504a250
TranDucKhiem14082005 <khiemtran015@gmail.com>
2024-09-21 00:00:02 +0700

    review

--- On_tap/review-js/main.js
+++ On_tap/review-js/main.js
@@ -19,13 +19,13 @@
 
 */
 
-var fullName = 'Tran Duc Khiem';
+// var fullName = 'Tran Duc Khiem';
 
-console.log(fullName);
+// console.log(fullName);
 
-setTimeout(function () {
-    console.log(fullName)
-},1000)
+// setTimeout(function () {
+//     console.log(fullName)
+// },1000)
 
 
 /*
@@ -48,7 +48,125 @@ setTimeout(function () {
     --
 */
 
-const a = 1;
-const b = 2;
-const c = a + b;
-console.log(c);
+// const a = 1;
+// const b = 2;
+// const c = a + b;
+// console.log(c);
+
+
+/*
+Toán tử chuỗi
+*/
+
+// var firstName = 'Khiem';
+// var lastName = 'Tran';
+
+// console.log(firstName + ' '+ lastName);
+
+/*
+Toán tử so sánh
+== --> Bằng
+!= --> Không bằng
+> --> Lớn hơn
+< --> Bé hơn
+>= --> Lớn hơn hoặc bằng
+<= --> Bé hơn hoặc bằng
+*/
+
+// var a = 1;
+// var b = 2;
+// if( a != b) {
+//     console.log('Điều kiện đúng');
+// }else 
+//     console.log('Điều kiện sai');
+
+/*
+*Boolean
+*/
+
+// var a = 1;
+// var b = 2;
+// var Success = a > b;
+// console.log(Success);
+
+/**
+ * If - else
+//  */
+// const a = 4;
+// const b = 4
+// if (a >= b) {
+//     console.log('DIEU KIEN DUNG');
+// }else {
+//     console.log('DIEU KIEN SAI');
+// }
+
+/**
+ * Những value này điều là sai, ngoài những giá trị này thì nó luôn đúng
+ * 
+ * 0
+ * false
+ * '' _ ""
+ * undefined
+ * NaN
+ * null
+ */
+
+/**
+ * Toán tử logic
+ * 1. && - And: all condition the true is true
+ * 2. || - Or: one in all condition the true is true
+ * 3. ! - Not
+ */
+
+var a = 1;
+var b = 3;
+var c = 6;
+if(a>0 && b >0){
+    console.log('true');
+}
+
+/*
+Data type in JavaScript
+
+1. Dữ liệu nguyên thử -  Primitive Data
+giá trị được tạo ra và được lưu vào vùng nhớ và không được thay đổi
+giá trị/vùng nhớ đó
+    -Number
+    -String
+    -Boolean:  true / false
+    -Undefined: khi biến được tạo và không gán giá trị cho nó
+    -Null: Không có gì
+    -Symboll:
+
+2. Dữ liệu phức tạp - Complex Data
+    -Function: function () {}
+    -Object: {}
+*/
+
+// Symboll
+
+var id = Symbol('id'); //unique
+
+var id2= Symbol('id');
+
+if(id == id2){
+    console.log(true);
+}
+
+var myObject = {
+    name: 'Khiem Tran',
+    age: 19,
+    adress: 'Nha Trang',
+    myFunction: function () {
+
+    }
+}
+
+var myArray = [ //Array là một đứa con trong Object
+    //dinh nghia mot danh sach
+    'JavaScript',
+    'PHP',
+    'Ruby'
+];
+
+// how to check data type in JavaScript; typeof <Tên biến>
