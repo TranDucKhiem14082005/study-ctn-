7de1726d490512fb9ba1eed139e50b2e2f9cb2af
TranDucKhiem14082005 <khiemtran015@gmail.com>
2024-10-10 11:37:41 +0700

    review: innerHTML_DOM

--- On_tap/review-js/main2.js
+++ On_tap/review-js/main2.js
@@ -39,51 +39,51 @@ DOM: là đại diện cho cả website => truy cập vào Element,attribute,tex
 // Element: ID, class, tag Name (h1,h2,...),
 //CSS selector, HTML collection
 
-let headingNode = document.getElementById('heading');
-console.log(headingNode);
+    // let headingNode = document.getElementById('heading');
+    // console.log(headingNode);
 
-// 1 trang web chỉ có 1 ID duy nhất
-// Nếu đưa vào ID sai => Null
-let headingNodes = document.getElementsByClassName('heading');
-console.log(headingNodes);
-// ->Trả về HTML collection => có thể sử dụng vòng lặp, lặp qua những element trong đó
+    // // 1 trang web chỉ có 1 ID duy nhất
+    // // Nếu đưa vào ID sai => Null
+    // let headingNodes = document.getElementsByClassName('heading');
+    // console.log(headingNodes);
+    // // ->Trả về HTML collection => có thể sử dụng vòng lặp, lặp qua những element trong đó
 
 
 
-let headingNode2s = document.getElementsByTagName('p'); // -> select ra thẻ
-console.log(headingNode2s);
+    // let headingNode2s = document.getElementsByTagName('p'); // -> select ra thẻ
+    // console.log(headingNode2s);
 
-//CSS Selecter
+    // //CSS Selecter
 
-let headingNode3s = document.querySelectorAll('.box .heading-2');
-// document.querySelector => trả về đúng thằng mình muốn
+    // let headingNode3s = document.querySelectorAll('.box .heading-2');
+    // // document.querySelector => trả về đúng thằng mình muốn
 
-//-> trả về nodelist (mảng)
-console.log(headingNode3s); 
+    // //-> trả về nodelist (mảng)
+    // console.log(headingNode3s); 
 
-//HTML collection
-console.log(document.forms['form-3']);
-console.log(document.forms.testForm);
+    // //HTML collection
+    // console.log(document.forms['form-3']);
+    // console.log(document.forms.testForm);
 
-//document.anchors => trả về a có attribute là name'
+    // //document.anchors => trả về a có attribute là name'
 
-//Cach 1
-let listItemNodes = 
-    document.querySelectorAll('.box-1 li')
+    // //Cach 1
+    // let listItemNodes = 
+    //     document.querySelectorAll('.box-1 li')
 
-console.log(listItemNodes);
+    // console.log(listItemNodes);
 
-let boxNode = 
-    document.querySelector('.box-1');
+    // let boxNode = 
+    //     document.querySelector('.box-1');
 
-//Công việc 1: sử dụng tới `boxNode`
+    // //Công việc 1: sử dụng tới `boxNode`
 
-// CÔng việc 2: sử dụng tới li element là con của `box.1`
-console.log(boxNode);
+    // // CÔng việc 2: sử dụng tới li element là con của `box.1`
+    // console.log(boxNode);
 
-console.log(boxNode.querySelectorAll('li'));
-console.log(boxNode.getElementsByTagName('p'));
-console.log(boxNode.querySelector('p'));
+    // console.log(boxNode.querySelectorAll('li'));
+    // console.log(boxNode.getElementsByTagName('p'));
+    // console.log(boxNode.querySelector('p'));
 
 
 /*
@@ -91,7 +91,64 @@ console.log(boxNode.querySelector('p'));
 2. getElementByClassName: -> HTML collection
 3. getElementByTagName:-> HTML collection
 4. querySelector: Trả về trực tiếp element
-5. querySelectorAll: -> HTML collection
+5. querySelectorAll: -> HTML collection 
 6: HTML collection 
 7. document.write
 */
+
+// ================================== //
+
+/**
+ * Text node in DOM
+ * 
+ * InnerText && textContent Property
+ * => Lấy ra được nội dung hoặc sửa đổi của Text Node trong element
+ * 
+ * geter : lấy 
+ * 
+ * seter : đặt / thay đổi
+ * 
+ * inerText: nếu sử dụng methods geter thì nó sẽ lấy đúng cái
+ * // giá trị mà nó đang hiện lên trình duyệt
+ * 
+ * textContent: Trả về nội dung của nó trong DOM
+ */
+
+
+// let headingElement = 
+//     document.querySelector('.heading');
+
+
+// console.log(headingElement.innerText); //Geter   
+// // headingElement.innerText = 'New heading'; // thay đổi nội dung
+// console.log(headingElement.textContent);
+
+// headingElement.textContent = 'New heading'; //Set ter
+
+// =============== //
+
+// Thêm một element vào trong một element đã có sẵn
+// ví dụ như, trong một thẻ div có class = box bây giờ mình muốn
+//Thêm một thẻ h1 có class = name vào trong thẻ div nữa thì mình phải
+// làm sao
+
+
+let boxElement = document.querySelector('.box');
+
+// ** Inner
+
+// boxElement.innerHTML = '<h1 title = "Heading">New Heading</h1>' ; //set
+
+// console.log(boxElement.innerHTML); // get
+
+// console.log(document.querySelector('h1').innerText);
+
+
+//Outer
+// console.log(boxElement.outerHTML); // get
+// 
+// Set
+
+boxElement.outerHTML = '<span>test</span>' ;
+
+console.log(boxElement); // còn nhưng trong bộ nhớ => DOM k còn
